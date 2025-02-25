7b3b942209c54c6ed5c7137535e1a5455c980d99
Prabhas Golla <prabhashgolla@gmail.com>
2025-02-25 18:19:57 +0530

    Added Formating in Frontend

--- Assignment4/frontend/src/App.jsx
+++ Assignment4/frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState,useEffect } from 'react'
 
 function App() {
+  const [ai,setAi] = useState([]);
+
   useEffect(()=>{
     async function fetchData(){
     console.log(import.meta.env.VITE_API_URL)
@@ -16,6 +18,7 @@ function App() {
       }
       const result = await response.json();
       console.log(result);
+      setAi(result);
     } catch(e){
       console.log("Error Fetching the data:",e);
     }
@@ -26,7 +29,12 @@ function App() {
 
   return (
     <>
-    
+      <h1>Data from API:</h1>
+      <ul>
+        {ai.map((item, index) => (
+          <li key={index}>{JSON.stringify(item)}</li>
+        ))}
+      </ul>
     </>
   )
 }
--- Assignment4/frontend/src/main.jsx
+++ Assignment4/frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <App />
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<App />}/>
+    </Routes>
+  </Router>
   // </StrictMode>,
 )
